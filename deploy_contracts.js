755ec3f2dba24fd4b3f783c87ca49c317e329ceb
EasyLose <160155157+EasyLose@users.noreply.github.com>
2024-04-15 18:58:39 +0200

    clean code

--- deploy_contracts.js
+++ deploy_contracts.js
@@ -1,17 +1,15 @@
 const DigitalAssetTracker = artifacts.require("DigitalAssetTracker");
 
-module.exports = function (deployer, network, accounts) {
-    if(network === 'development') {
-        deployer.deploy(DigitalAssetTracker).then(() => {
-            console.log("DigitalAssetTracker deployed on development network");
-        });
-    } else if(network === 'ropsten' || network === 'mainnet') {
-        deployer.deploy(DigitalAssetTracker).then(() => {
-            console.log(`DigitalAssetTracker deployed on ${network} network`);
-        });
-    } else {
-        deployer.deploy(DigitalAssetTracker).then(() => {
-            console.log(`DigitalAssetTracker deployed on unknown or unspecified network (${network})`);
-        });
-    }
+module.exports = function(deployer, network) {
+    deployer.deploy(DigitalAssetTracker).then(() => {
+        let networkMessage = "unknown or unspecified";
+
+        if (network === 'development') {
+            networkMessage = "development";
+        } else if (network === 'ropsten' || network === 'mainnet') {
+            networkMessage = network;
+        }
+
+        console.log(`DigitalAssetTracker deployed on ${networkMessage} network`);
+    });
 };
