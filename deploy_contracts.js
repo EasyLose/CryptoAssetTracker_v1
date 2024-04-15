9050a6d1ace30b559b30d98cadf1f60145623f29
EasyLose <160155157+EasyLose@users.noreply.github.com>
2024-04-15 19:01:15 +0200

    new update

--- deploy_contracts.js
+++ deploy_contracts.js
@@ -1,7 +1,17 @@
 const DigitalAssetTracker = artifacts.require("DigitalAssetTracker");
 
-module.exports = function(deployer, network) {
-    deployer.deploy(DigitalAssetTracker).then(() => {
+async function getCurrentGasPrice() {
+    let gasPrice = await web3.eth.getGasPrice();
+    gasPrice = web3.utils.toBN(gasPrice);
+    const adjustedGasPrice = gasPrice.mul(web3.utils.toBN(1.1)); // 10% more than the average
+    return adjustedGasPrice.toString();
+}
+
+module.exports = async function(deployer, network) {
+    const gasPrice = await getCurrentGasPrice(); // Fetch current average gas price with a slight increase
+    console.log(`Current adjusted gas price for deployment: ${gasPrice}`);
+
+    deployer.deploy(DigitalAssetTracker, {gasPrice: gasPrice}).then(() => {
         let networkMessage = "unknown or unspecified";
 
         if (network === 'development') {
@@ -10,6 +20,6 @@ module.exports = function(deployer, network) {
             networkMessage = network;
         }
 
-        console.log(`DigitalAssetTracker deployed on ${networkMessage} network`);
+        console.log(`DigitalAssetTracker deployed on ${networkMessage} network with gas price ${gasPrice}`);
     });
 };
