37300e64e7abad7da650540d9262da11a3d65495
EasyLose <160155157+EasyLose@users.noreply.github.com>
2024-04-15 18:38:49 +0200

    optimization update

--- contracts/AssetsContract.sol
+++ contracts/AssetsContract.sol
@@ -3,27 +3,27 @@ pragma solidity ^0.8.0;
 
 contract DigitalAssetTracker {
     struct Asset {
-        uint id;
+        uint32 id;
         address owner;
         string name;
         string description;
         bool exists;
     }
-    uint private nextAssetId;
-    mapping(uint => Asset) public assets;
-    mapping(uint => address[]) public assetHistory;
-    mapping(address => uint[]) public ownerAssets;
+    uint32 private nextAssetId = 1;
+    mapping(uint32 => Asset) public assets;
+    mapping(uint32 => address[]) public assetHistory;
+    mapping(address => uint32[]) public ownerAssets;
     mapping(address => bool) public authorizedAccounts;
-    mapping(uint => address) public pendingTransfers;
+    mapping(uint32 => address) public pendingTransfers;
 
-    event AssetRegistered(uint assetId, address owner, string name, string description);
-    event AssetTransferred(uint assetId, address from, address to);
-    event AssetRemoved(uint assetId);
+    event AssetRegistered(uint32 assetId, address owner, string name, string description);
+    event AssetTransferred(uint32 assetId, address from, address to);
+    event AssetRemoved(uint32 assetId);
     event AuthorizationChanged(address account, bool isAuthorized);
-    event TransferInitiated(uint assetId, address from, address to);
-    event TransferAccepted(uint assetId, address to);
+    event TransferInitiated(uint32 assetId, address from, address to);
+    event TransferAccepted(uint32 assetId, address to);
 
-    modifier onlyOwner(uint assetId) {
+    modifier onlyOwner(uint32 assetId) {
         require(assets[assetId].exists, "Asset does not exist.");
         require(assets[assetId].owner == msg.sender, "Not the asset owner.");
         _;
@@ -34,7 +34,7 @@ contract DigitalAssetTracker {
         _;
     }
 
-    modifier onlyPendingTransfer(uint _assetId, address _to) {
+    modifier onlyPendingTransfer(uint32 _assetId, address _to) {
         require(pendingTransfers[_assetId] == _to, "No pending transfer to this address for this asset.");
         _;
     }
@@ -45,46 +45,45 @@ contract DigitalAssetTracker {
     }
 
     function registerAsset(string memory _name, string memory _description) public onlyAuthorized {
-        assets[nextAssetId] = Asset(nextAssetId, msg.sender, _name, _description, true);
-        assetHistory[nextAssetId].push(msg.sender);
-        ownerAssets[msg.sender].push(nextAssetId);
-        emit AssetRegistered(nextAssetId, msg.sender, _name, _description);
-        nextAssetId++;
+        uint32 assetId = nextAssetId++;
+        assets[assetId] = Asset(assetId, msg.sender, _name, _description, true);
+        assetHistory[assetId].push(msg.sender);
+        ownerAssets[msg.sender].push(assetId);
+        emit AssetRegistered(assetId, msg.sender, _name, _description);
     }
 
-    function transferAsset(uint _assetId, address _newOwner) public onlyOwner(_assetId) {
+    function transferAsset(uint32 _assetId, address _newOwner) public onlyOwner(_assetId) {
         pendingTransfers[_assetId] = _newOwner;
         emit TransferInitiated(_assetId, msg.sender, _newOwner);
     }
 
-    function acceptTransfer(uint _assetId) public onlyPendingTransfer(_assetId, msg.sender) {
+    function acceptTransfer(uint32 _assetId) public onlyPendingTransfer(_assetId, msg.sender) {
         address oldOwner = assets[_assetId].owner;
         assets[_assetId].owner = msg.sender;
         assetHistory[_assetId].push(msg.sender);
         ownerAssets[msg.sender].push(_assetId);
         delete pendingTransfers[_assetId];
         emit AssetTransferred(_assetId, oldOwner, msg.sender);
+        // Removing an asset from the previous owner's array is not handled here.
     }
 
-    function removeAsset(uint _assetId) public onlyOwner(_assetId) {
+    function removeAsset(uint32 _assetId) public onlyOwner(_assetId) {
         delete assets[_assetId];
-        delete ownerAssets[msg.sender][_assetId];
-        // Optionally clear history or keep it for record purposes
-        // delete assetHistory[_assetId];
+        // Direct deletion in an array of `ownerAssets[msg.sender][_assetId]` is not possible; needs complex logic to manage.
         emit AssetRemoved(_assetId);
     }
 
-    function getAsset(uint _assetId) public view returns (Asset memory) {
+    function getAsset(uint32 _assetId) public view returns (Asset memory) {
         require(assets[_assetId].exists, "Asset does not exist.");
         return assets[_assetId];
     }
 
-    function getAssetHistory(uint _assetId) public view returns (address[] memory) {
+    function getAssetHistory(uint32 _assetId) public view returns (address[] memory) {
         require(assets[_assetId].exists, "Asset does not exist.");
         return assetHistory[_assetId];
     }
 
-    function getOwnerAssets(address _owner) public view returns (uint[] memory) {
+    function getOwnerAssets(address _owner) public view returns (uint32[] memory) {
         return ownerAssets[_owner];
     }
 
