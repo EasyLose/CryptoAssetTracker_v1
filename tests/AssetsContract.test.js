f29712637c8448026c89a73c81e29bedfa81a95d
EasyLose <160155157+EasyLose@users.noreply.github.com>
2024-04-15 19:00:14 +0200

    clean code

--- tests/AssetsContract.test.js
+++ tests/AssetsContract.test.js
@@ -1,19 +1,19 @@
 const AssetLedger = artifacts.require("AssetLedger");
 
-contract("AssetLedger", accounts => {
+contract("AssetLedger", (accounts) => {
     const [admin, participantA, participantB] = accounts;
 
     before(async () => {
         this.ledger = await AssetLedger.deployed();
     });
 
-    it("Allows authorized users to register a new asset", async () => {
+    it("allows authorized users to register a new asset", async () => {
         await this.ledger.authorizeUser(participantA, true, { from: admin });
         const transaction = await this.ledger.registerNewAsset("Asset1", "Description1", { from: participantA });
         assert.equal(transaction.logs[0].event, "AssetRegistered", "The asset should be registered successfully");
     });
 
-    it("Prevents unauthorized users from registering a new asset", async () => {
+    it("prevents unauthorized users from registering a new asset", async () => {
         try {
             await this.ledger.registerNewAsset("Asset2", "Description2", { from: participantB });
             assert.fail("An unauthorized user was able to register a new asset");
@@ -22,13 +22,13 @@ contract("AssetLedger", accounts => {
         }
     });
 
-    it("Enables the asset owner to initiate an asset transfer", async () => {
+    it("enables the asset owner to initiate an asset transfer", async () => {
         await this.ledger.registerNewAsset("Asset3", "Description3", { from: participantA });
         const transaction = await this.ledger.initiateTransfer(1, participantB, { from: participantA });
         assert.equal(transaction.logs[0].event, "TransferInitiated", "The transfer initiation should be recorded");
     });
 
-    it("Blocks non-owners from initiating an asset transfer", async () => {
+    it("blocks non-owners from initiating an asset transfer", async () => {
         try {
             await this.ledger.initiateTransfer(1, participantB, { from: participantB });
             assert.fail("A non-owner was able to initiate an asset transfer");
@@ -37,13 +37,13 @@ contract("AssetLedger", accounts => {
         }
     });
 
-    it("Allows the recipient to complete the asset transfer", async () => {
+    it("allows the recipient to complete the asset transfer", async () => {
         await this.ledger.completeTransfer(1, { from: participantB });
         const assetDetails = await this.ledger.retrieveAssetDetails(1);
         assert.equal(assetDetails.owner, participantB, "The asset ownership was not transferred correctly");
     });
 
-    it("Restricts asset removal to unauthorized users", async () => {
+    it("restricts asset removal to unauthorized users", async () => {
         try {
             await this.ledger.removeAsset(1, { from: admin });
             assert.fail("An unauthorized user was able to remove the asset");
@@ -52,12 +52,12 @@ contract("AssetLedger", accounts => {
         }
     });
 
-    it("Permits the asset owner to remove the asset", async () => {
+    it("permits the asset owner to remove the asset", async () => {
         const transaction = await this.ledger.removeAsset(1, { from: participantB });
         assert.equal(transaction.logs[0].event, "AssetRemoved", "The asset removal should be recorded in the event log");
     });
 
-    it("Prohibits unauthorized users from altering authorization settings", async () => {
+    it("prohibits unauthorized users from altering authorization settings", async () => {
         try {
             await this.ledger.authorizeUser(participantB, true, { from: participantA });
             assert.fail("An unauthorized user was able to modify authorization settings");
