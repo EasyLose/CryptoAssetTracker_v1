34fc6915742787eb4f6a49720cc3cf247ebb9eeb
EasyLose <160155157+EasyLose@users.noreply.github.com>
2024-04-15 19:22:53 +0200

    clean code

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 let cryptoAssets = [
     { id: "btc", name: "Bitcoin", priceUSD: 20000 },
     { id: "eth", name: "Ethereum", priceUSD: 1500 },
-    { id: "ltc", name: "Litecoin", priceUSD: 100 }
+    { id: "ltc", name: "Litecoin", priceUSD: 100 },
 ];
 
+function getCryptoById(cryptoId) {
+    return cryptoAssets.find(asset => asset.id === cryptoId);
+}
+
 function displayCryptoPrice(cryptoId) {
-    const crypto = cryptoAssets.find(asset => asset.id === cryptoId);
+    const crypto = getCryptoById(cryptoId);
     if (crypto) {
         console.log(`The price of ${crypto.name} is $${crypto.priceUSD}.`);
     } else {
@@ -14,7 +18,7 @@ function displayCryptoPrice(cryptoId) {
 }
 
 function updateCryptoPrice(cryptoId, newPrice) {
-    const crypto = cryptoAssets.find(asset => asset.id === cryptoId);
+    const crypto = getCryptoById(cryptoId);
     if (crypto) {
         crypto.priceUSD = newPrice;
         console.log(`${crypto.name}'s price has been updated to $${newPrice}.`);
